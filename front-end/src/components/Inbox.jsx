f7dec16017d7f5627d11f7baf1db4858b1517e68
Sanchit <sanchit04004@gmail.com>
2024-02-10 16:23:43 +0530

    A new commit

--- front-end/src/components/Inbox.jsx
+++ front-end/src/components/Inbox.jsx
@@ -4,7 +4,7 @@ export const Inbox =({label}) => {
   <>
     <div className='flex justify-center'>
 
-    <div className='bg-hotred w-64 h-9 mt-2  rounded-lg'>
+    <div className='bg-hotred text-white w-64 h-9 mt-2  rounded-lg'>
         {label}
     </div>
     </div>
