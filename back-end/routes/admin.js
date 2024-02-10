2aafd073acdc015dda12d122eb6ad7399a13ecbf
Rithk <rithkchaudhary86@gmail.com>
2024-02-10 15:35:54 +0530

    admin signup

--- back-end/routes/admin.js
+++ back-end/routes/admin.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Admin } = require("../database/db");
 const router = express.Router();
 
 
@@ -7,4 +8,23 @@ router.get("/",(req,res)=>{
         msg:"admin route working fine"
     })
 })
+
+router.post("/signup",async(req,res)=>{
+    const body = req.body;
+    const newUser = await Admin.create({
+        username: body.username,
+        password: body.password
+    })
+    if(newUser){
+        res.json({
+            msg: "Admin created Successfully"
+        })
+    }
+    else{
+        res.status(500).json({
+            msg: "Internal Server error"
+        })
+    }
+})
+
 module.exports=router; 
