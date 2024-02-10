12a3326dee491e3f95924adf7b5019d8240cdd9a
Japneet Singh <meridiary.062022@gmail.com>
2024-02-10 16:10:36 +0530

    Committing changes in App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-// import Footer from './components/Footer'
-// import CheckDocuments from './components/CheckDocuments'
-import {Inbox} from './components/Inbox'
+
 
 const App = () => {
-  return <>
-  <div>
-{/* <CheckDocuments/>
- <Footer/> */}
- <Inbox label={"Home"}/>
- <Inbox label={"Inbox"}/>
- <Inbox label={"Login"}/>
-  </div>
+  return 
+  <>
+
   </>
 }
 
