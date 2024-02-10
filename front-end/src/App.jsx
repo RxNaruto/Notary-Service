aae4b4279b1f08eb8988696234c4c4d56a1b6a18
Sanchit <sanchit04004@gmail.com>
2024-02-10 17:07:02 +0530

    False commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,19 @@
-
+import {CheckDocuments} from "./components/CheckDocuments";
+import { FooterLinks } from "./components/FooterLinks";
+import{ Inbox } from"./components/Inbox";
 const App=()=>{
   return (
     <>
     
-    </>
+    <div className="bg-black min-h-screen">
+    <div className="flex justify-evenly text-center ">
+      <CheckDocuments label={"Check Documents"}/>
+      <Inbox label={"Home"}/>
+      <Inbox label={"Inbox"}/>
+      <Inbox label={"Login"}/>
+      </div>
+      </div>
+      </>
   )
 }
 export default App;
