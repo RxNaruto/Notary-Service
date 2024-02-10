780f549e86f9a93be737bed6632949a768f53c5b
Japneet Singh <meridiary.062022@gmail.com>
2024-02-10 15:12:49 +0530

    footer created inbox made

---
+++ front-end/src/components/FooterLinks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterLinks = ({label}) => {
+  return (
+    
+    <div className='text-hotred pt-4 relative underline flex justify-center cursor-pointer'>
+        {label}
+        </div>
+    
+  )
+}
+
+export default FooterLinks
