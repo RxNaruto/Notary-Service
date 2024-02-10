25b9e78aa19fd2e5029e7816bd0f501eaa138c7c
Parshant <wewillsuceed444@gmail.com>
2024-02-10 15:53:28 +0530

    notary services and types are added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,17 @@
+import { NotaryServices } from "./components/NotaryServices";
+import { NotaryTypes } from "./components/NotaryTypes";
+
 const App=()=>{
-  return <>
- 
+  return( <>
   
+      <NotaryServices label={"Notary Services"}/>
+      <NotaryTypes label={"Affidavits"}/>
+      <NotaryTypes label={"Real Estate Deals"}/>
+      <NotaryTypes label={"Car "}/>
+
   </>
+  )
 }
+
 
 export default App;
